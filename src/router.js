af9fef43da5951b6342cbcc52e915bf19864746a
maziardehghani <maziardehghani1380@gmail.com>
2025-03-29 07:07:07 +0330

    add register page

--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from "./pages/Home.vue";
 import Index from "./pages/Tasks/Index.vue";
 import Login from "./pages/Auth/Login.vue";
+import Register from "./pages/Auth/Register.vue";
 
 
 const routes = [
     {path: '/home', component: Home, name:'home'},
     {path: '/tasks', component: Index, name:'tasks'},
     {path: '/login', component: Login, name:'login'},
+    {path: '/register', component: Register, name:'register'},
 ]
 
 const router = createRouter({
